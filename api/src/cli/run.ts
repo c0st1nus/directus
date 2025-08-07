7c40e3041a61a549cc3c34acf2e25d0e6de9924c
C0nstanta <konstantin.koshevoj@bk.ru>
2025-08-07 17:46:14 +0500

    Run fix

--- api/src/cli/run.ts
+++ api/src/cli/run.ts
@@ -1,9 +1,16 @@
 import { createCli } from './index.js';
 
+let args = process.argv;
+
+if (process.argv[1] && process.argv[1].includes('ProcessContainer.js')) {
+    const directusCommand = process.argv[process.argv.length - 1] || '';
+    args = [process.argv[0] || '', process.argv[1] || '', directusCommand];
+}
+
 createCli()
-	.then((program) => program.parseAsync(process.argv))
-	.catch((err) => {
-		// eslint-disable-next-line no-console
-		console.error(err);
-		process.exit(1);
-	});
+    .then((program) => program.parseAsync(args))
+    .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
+        process.exit(1);
+    });
